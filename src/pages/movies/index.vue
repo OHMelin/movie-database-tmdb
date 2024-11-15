f565c809105d9ec832f7c6792279e29e99d2efcd
OHMelin <oliverholstmelin@gmail.com>
2024-11-15 16:14:48 +0100

    Add basic movie slider view styling

--- src/pages/movies/index.vue
+++ src/pages/movies/index.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="wrapper">
-    <div class="flex justify-center">
-      <h1 class="font-bold text-3xl">Find Tonights Movie</h1>
+  <div class="outer-wrapper">
+    <div class="wrapper">
+      <h1 class="font-bold text-5xl">Find Tonights Movie</h1>
     </div>
+  </div>
+  <div class="wrapper">
     <div v-for="genre in genreListData" :key="genre.id">
-      <p>{{ genre.name }} - (<NuxtLink :to="'/movies/genre/' + genre.id">{{ genre.totalMovies }} movies in this genre</NuxtLink>)</p>
-      <div v-for="movie in genre.movies" :key="movie.id">
-        <nuxtLink :to="'/movies/' + movie.id">{{ movie.title }}</nuxtLink>
-      </div>
+      <p class="font-bold text-2xl">{{ genre.name }} - (<NuxtLink :to="'/movies/genre/' + genre.id">{{ genre.totalMovies }} movies in this genre</NuxtLink>)</p>
+      <v-sheet class="mx-auto">
+        <v-slide-group show-arrows>
+          <v-slide-group-item
+            v-for="movie in genre.movies"
+            :key="movie.id"
+          >
+            <NuxtLink :to="'/movies/' + movie.id" class="m-2 w-[200px]">
+              <v-card
+                color="grey-lighten-1"
+                height="300"
+                width="200"
+                :style="{
+                  backgroundImage: `url(https://image.tmdb.org/t/p/w200${movie.poster_path})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat'
+                }"
+              >
+              </v-card>
+              <p class="whitespace-nowrap text-ellipsis overflow-hidden text-sm">{{ movie.title }}</p>
+            </NuxtLink>
+          </v-slide-group-item>
+        </v-slide-group>
+      </v-sheet>
     </div>
   </div>
 </template>
@@ -42,3 +65,14 @@ onMounted(() => {
   fetchGenreListData();
 });
 </script>
+
+<style scoped>
+.outer-wrapper {
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('../../assets/test.webp');
+  background-size: cover;
+  background-position: center;
+  text-align: center;
+  color: white;
+  padding: 5rem;
+}
+</style>
